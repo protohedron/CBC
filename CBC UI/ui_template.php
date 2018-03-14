fe2d53adac003de438cac3132fad5f0d29381f35
AR97 <33634717+AR97@users.noreply.github.com>
2018-03-14 18:01:08 -0400

    CBC home page and UI elements. Also basic template for pages

---
+++ CBC UI/ui_template.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html lang = "en">
+
+<head>
+	<title>CBC</title>
+	<meta charset = "UTF-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="cbc.css">
+	<link rel="icon" type="image/png" href="Images/favicon.png">
+</head>
+
+<body>
+
+<!--Logo and nav holder-->
+<div class="top_bar_temp">
+	
+	<!--Logo holder-->
+	<div class="logo_holder">
+		<img class="logo" src="Images/logo.png"></img>
+	</div>
+	
+	<!--Nav bar-->
+	<nav>
+		<a href="" class="black">Ministries</a>
+		<a href="" class="black">Calendar</a>
+		<a href="" class="black">Missions</a>
+		<a href="" class="black">Online Giving</a>
+		<a href="" class="black">Sermons</a>
+		<a href="" class="black">About</a>
+	</nav>
+</div>
+
+<div class="header_bar teal">
+	<h1 class="header_title">Page Title Here (color will vary)</h1>
+</div>
+
+<h1 style="font-family: tweb; text-align: center; margin-top: 6em;">Content Here</h1>
+
+</body>
+</html>
