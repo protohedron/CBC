e5ba7cc01f696cb58798ae0c72bf5b632d9ccd72
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-03-18 16:31:29 -0400

    Home: Moves home.php to index.php in root directory.

---
+++ index.php
@@ -0,0 +1,170 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html lang = "en">
+
+<head>
+	<title>CBC</title>
+	<meta charset = "UTF-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="cbc.css">
+	<link rel="icon" type="image/png" href="Images/favicon.png">
+</head>
+
+<body>
+
+<!--Logo and nav holder-->
+<div class="top_bar">
+	
+	<!--Logo holder-->
+	<div class="logo_holder">
+		<img class="logo" src="Images/logo.png"></img>
+	</div>
+	
+	<!--Nav bar-->
+	<nav>
+		<a href="">Ministries</a>
+		<a href="">Calendar</a>
+		<a href="">Missions</a>
+		<a href="">Online Giving</a>
+		<a href="">Sermons</a>
+		<a href="">About</a>
+	</nav>
+</div>
+
+<!--Showcase Image-->
+<img class="shcase_img" src="Images/img_1.png"></img>
+
+<!--Begin Sections-->
+<div class="home_grid">
+
+	<!--Begin Get To know us Section-->
+	<div class="section_block lightred">
+		<h1 class="section_title">Get To Know Us</h1>
+	</div>
+	
+	<div class="info_block lightred">
+		<h2 class="filler_text">Here at CBC our mission is to produce followers of Jesus Christ who join Him in loving God, loving people and redeeming the world<br>[Then maybe a tad bit more about the Church]<br>[Also maybe put address here?]</h2>
+	</div>
+	<!--End Get To Know Us-->
+	
+	
+	<!--Begin Service Times Section-->
+		<div class="info_block darkgray">
+		<h2 class="filler_text">Sunday Morning: 9:30am<br>Sunday Night: [time]<br> Wednesday Night: [time]</h2>
+	</div>
+	
+	<div class="section_block darkgray" id="two">
+		<h1 class="section_title">Service Times</h1>
+	</div>
+	<!--End Service Times Section-->
+	
+	
+	<!--Begin Upcoming Events Section-->
+	<div class="section_block teal">
+		<h1 class="section_title">Upcoming Events</h1>
+	</div>
+	
+	<div class="info_block teal">
+		<h2 class="filler_text">Event#1 - [date]<br>Event#2 - [date]<br> Event#3 - [date]<br><br><u>More</u></h2>
+	</div>
+	<!--End Upcoming Events Section-->
+	
+	
+	<!--Begin Missions Section-->
+		<div class="info_block purplegray">
+		<h2 class="filler_text">Community Bible Church helps to support over a dozen missionaries across the world and has been a part of several church plants.<br><br><u>See More</u></h2>
+	</div>
+	
+	<div class="section_block purplegray" id="four">
+		<h1 class="section_title">Missions</h1>
+	</div>
+	<!--End Missions Section-->
+	
+	
+	<!--Begin Sermons Section-->
+	<div class="section_block mustard">
+		<h1 class="section_title">Sermon Podcasts</h1>
+	</div>
+	
+	<div class="info_block mustard">
+		<h2 class="filler_text">CBC is passionate about producing genuine followers of Jesus Christ who join Him
+								in LOVING GOD, LOVING PEOPLE, and REDEEMING THE WORLD. Solid Biblical teaching and preaching is a vital
+								component of that process. **Listen to Sermons wherever you are..**<br><br><u>Listen to Sermons</u></h2>
+	</div>
+	<!--End Sermons Section-->
+	
+	
+	<!--Ministries Section-->
+		<div class="info_block gray">
+		<h2 class="filler_text">Through ministries like 180 , GED, LifeTRUTH and many others, Community Bible Church is committed to Being Jesus to our community and making a difference in the lives of those around us.<br><br><u>See all Ministries</u></h2>
+	</div>
+	
+	<div class="section_block gray" id="six">
+		<h1 class="section_title">Ministries</h1>
+	</div>
+	<!--End Ministries Section-->
+	
+	
+	<!--Begin Online Giving Section-->
+	<div class="section_block darkred">
+		<h1 class="section_title">Online Giving</h1>
+	</div>
+	
+	<div class="info_block darkred">
+		<h2 class="filler_text">CBC is excited to offer the option of giving online with a credit/debit card or through a direct checking account withdrawal. Your gift will be processed promptly, and your financial information will be secure. May God bless you as you follow His direction in your giving!<br><br><u>Give Online</u></h2>
+	</div>
+	<!--End Online Giving Section-->
+	
+	
+	<!--I'm New Section-->
+		<div class="info_block bluegray">
+		<h2 class="filler_text">Whether this is your first time to Community Bible Church or you've been attending for a while, we want to hear from you.<br><br><u>Get Info</u></h2>
+	</div>
+	
+	<div class="section_block bluegray" id="eight">
+		<h1 class="section_title">I'm New</h1>
+	</div>
+	<!--End I'm New Section-->
+	
+	
+	<!--Begin Contact Section-->
+	<div class="section_block darkteal">
+		<h1 class="section_title">Contact and Info</h1>
+	</div>
+	
+	<div class="info_block darkteal">
+		<h2 class="filler_text">If you have any questions or would like to get plugged in at CBC please call us at 864-859-2662<br><br>Our address is <u>8005 Highway 81 North in Easley, SC</u></h2>
+	</div>
+	<!--End Contact Section-->
+	
+
+	<!--About, Staff & More Section-->
+	<div class="info_block lightgray">
+	<h2 class="filler_text">Learn about our staff, our values, our history, and more<br><br><u>Learn About CBC</u></h2>
+	</div>
+	
+	<div class="section_block lightgray" id="ten">
+		<h1 class="section_title">About CBC, Staff &amp; More</h1>
+	</div>
+	<!--End About, Staff & More Section-->
+	
+	
+	<!--Begin MyCBC-->
+	<div class="section_block" style="background-color: #414118">
+		<h1 class="section_title">Social Media</h1>
+	</div>
+	
+	<div class="info_block" style="background-color: #414118">
+		<h2 class="filler_text">Facebook Instagram and YouTube</u></h2>
+	</div>
+	<!--End MyCBC Section-->	
+</div>
+
+<!--Footer-->
+<footer></footer>
+
+</body>
+</html>
